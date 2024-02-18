888f12cebf490ae00a47451bdcb2b35d35c396bd
Maurice Daniel V. Hiponia <maurice.hiponia@gmail.com>
2024-02-18 16:44:30 +0800

    Optimize Images,Profile, Preload Script

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-let btn_prev = document.querySelector(".button-prev"); 
-let btn_next = document.querySelector(".button-next"); 
-
+let btn_prev = document.querySelector(".button-prev");
+let btn_next = document.querySelector(".button-next");
 let screenshot = document.querySelector(".screenshot");
-
 let i = 1;
 
+const images = [];
+for (let j = 1; j <= 5; j++) {
+    const img = new Image();
+    img.src = `./Images/image${j}.png`;
+    images.push(img);
+}
 btn_next.addEventListener("click", function(e) {
-    if(i < 5)
-    {
+    if (i < 5) {
         i++;
-        screenshot.src=`./Images/image${i}.png`;
+        screenshot.src = images[i - 1].src;
     }
 });
 
 btn_prev.addEventListener("click", function(e) {
-    if(i > 1)
-    {
+    if (i > 1) {
         i--;
-        screenshot.src=`./Images/image${i}.png`;
+        screenshot.src = images[i - 1].src;
     }
 });
-
 
